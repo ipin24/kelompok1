a2843933261460cda9b868c89a87410a00479a65
cellosxyz <marcellofour@gmail.com>
2022-09-13 08:42:33 +0700

    menambahkan bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,47 @@
-import 'package:flutter/material.dart';
-import './launcher.dart';
+// ignore_for_file: use_key_in_widget_constructors, prefer_final_fields, prefer_const_constructors, avoid_function_literals_in_foreach_calls
 
+    import 'package:flutter/material.dart';
+import './launcher.dart';
+    
 void main() {
   runApp(const MyApp());
 }
-
-class MyApp extends StatelessWidget {
+    
+    class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
+    
   // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
+      @override
+      Widget build(BuildContext context) {
     return MaterialApp(
       title: 'MyCanteen',
       theme: ThemeData(
         primarySwatch: Colors.green,
-      ),
+                  ),
       home: LauncherPage(),
-    );
-  }
-}
+        );
+      }
+    
+      //fungsi ini akan dijalankan setiap kalian typing/mengetik
+      //fungsi ini akan mencari nama sesuai yang diketikkan
+      _searchName(String name) {
+        if (name.isNotEmpty) {
+          setState(() {
+            list.clear();
+            //melakukan perulangan/looping
+            items.forEach((item) {
+              //jika list data ada yang mengandung susunan huruf yang dicari
+              //maka masukan ke dalam data list
+              if (item.toLowerCase().contains(name.toLowerCase())) {
+                list.add(item);
+              }
+            });
+          });
+        } else {
+          setState(() {
+            list.clear();
+            list.addAll(items);
+          });
+        }
+      }
+    }
